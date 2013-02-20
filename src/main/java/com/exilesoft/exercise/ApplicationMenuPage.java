abb669aabfad86dc9b430dccb804c020fe11c8b9
Johannes Brodwall <johannes@brodwall.com>
2013-02-20 18:18:48 +0100

    Now using Spring (clumsily) from Wicket

--- src/main/java/com/exilesoft/exercise/ApplicationMenuPage.java
+++ src/main/java/com/exilesoft/exercise/ApplicationMenuPage.java
@@ -1,7 +1,27 @@
 package com.exilesoft.exercise;
 
+import javax.servlet.ServletContext;
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.exilesoft.training.ApplicationInfo;
 
 public class ApplicationMenuPage extends WebPage {
 
+	public ApplicationMenuPage() {
+		add(new Label("title", getTitle()));
+		add(new Label("heading", getTitle()));
+	}
+
+	private String getTitle() {
+		ServletContext context = WebApplication.get().getServletContext();
+		WebApplicationContext applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(context);
+		ApplicationInfo applicationInfo = applicationContext.getBean(ApplicationInfo.class);
+
+		return applicationInfo.getName() + " :: Menu";
+	}
 }
--- src/main/java/com/exilesoft/training/ApplicationInfo.java
+++ src/main/java/com/exilesoft/training/ApplicationInfo.java
@@ -4,6 +4,10 @@ public class ApplicationInfo {
 
 	private String name;
 
+	public String getName() {
+		return name;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
