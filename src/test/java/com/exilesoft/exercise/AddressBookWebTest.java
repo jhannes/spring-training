1eaa8bcdb69da1f0aeb01be0f51b9657be0d6542
Johannes Brodwall <johannes@brodwall.com>
2013-02-22 15:04:31 +0100

    Streamlined starting of Jetty server

--- src/test/java/com/exilesoft/exercise/AddressBookWebTest.java
+++ src/test/java/com/exilesoft/exercise/AddressBookWebTest.java
@@ -3,13 +3,9 @@ package com.exilesoft.exercise;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.fail;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.jetty.plus.jndi.EnvEntry;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
 import org.h2.jdbcx.JdbcDataSource;
 import org.hibernate.cfg.AvailableSettings;
 import org.junit.Test;
@@ -18,26 +14,20 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import com.exilesoft.exercise.company.type.CompanyType;
-import com.exilesoft.exercise.company.type.CompanyTypeRepository;
 
 public class AddressBookWebTest {
 
     private WebDriver browser;
-	private String url;
-    private WebApplicationContext applicationContext;
+	private WebServer webServer;
 
     @Test
     public void fullScenarioTest() throws Exception {
-        this.url = startWebServer();
-        setApplicationName("Johannes's addresses");
-        setCompanyTypes("Software", "Offshoring");
+        startWebServer();
+        webServer.setApplicationName("Johannes's addresses");
+        webServer.setCompanyTypes("Software", "Offshoring");
         this.browser = createWebBrowser();
 
-        browser.get(url);
+        browser.get(webServer.getWebAppUrl());
         assertThat(browser.getTitle()).isEqualTo("Johannes's addresses :: Menu");
 
         addCompany("Visma", "http://www.visma.com/", "Software");
@@ -47,17 +37,6 @@ public class AddressBookWebTest {
 
         addPersonToCompany("Exilesoft", "Johannes Brodwall", "jbr@exilesoft.com");
         verifyPersonPresent("Johannes Brodwall (Exilesoft)");
-    }
-
-	private void setCompanyTypes(String... types) {
-        CompanyTypeRepository typeRepository = applicationContext.getBean(CompanyTypeRepository.class);
-        for (String type : types) {
-            typeRepository.create(new CompanyType(type));
-        }
-    }
-
-    private void setApplicationName(String applicationName) {
-        applicationContext.getBean(ApplicationInfo.class).setName(applicationName);
     }
 
     private void addCompany(String companyName, String companyUrl, String companyType) {
@@ -79,7 +58,7 @@ public class AddressBookWebTest {
 	}
 
 	private void verifyCompanyDetails(String company, String heading, String type) {
-	    browser.get(url);
+	    browser.get(webServer.getWebAppUrl());
 		navigateToCompany(company);
 	    assertThat(browser.findElement(By.id("heading")).getText()).isEqualTo(heading);
 	    assertThat(browser.findElement(By.id("type")).getText()).isEqualTo(type);
@@ -93,13 +72,13 @@ public class AddressBookWebTest {
 	}
 
 	private void verifyPersonPresent(String person) {
-        browser.get(url);
+        browser.get(webServer.getWebAppUrl());
         browser.findElement(By.linkText("List people")).click();
         assertThat(browser.findElement(By.id("people")).getText()).contains(person);
 	}
 
 	private void navigateToCompany(String company) {
-	    browser.get(url);
+	    browser.get(webServer.getWebAppUrl());
 		browser.findElement(By.linkText("List companies")).click();
 	    browser.findElement(By.linkText(company)).click();
 	}
@@ -115,22 +94,16 @@ public class AddressBookWebTest {
 	}
 
 	private String startWebServer() throws Exception {
-		System.out.println("Starting web server");
 	    System.setProperty("wicket.configuration", "DEPLOYMENT");
+	    System.setProperty(AvailableSettings.HBM2DDL_AUTO, "create");
 
+	    this.webServer = new WebServer(0, "/root");
 	    JdbcDataSource dataSource = new JdbcDataSource();
 	    dataSource.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
 	    dataSource.setUser("sa");
-	    new EnvEntry("java:/DefaultDS", dataSource);
-		System.setProperty(AvailableSettings.HBM2DDL_AUTO, "create");
-
-        Server server = new Server(0);
-        WebAppContext webApplication = new WebAppContext("src/main/webapp", "/root");
-        server.setHandler(webApplication);
-        server.start();
-
-        applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(webApplication.getServletContext());
-        return new URL("http", "localhost", server.getConnectors()[0].getLocalPort(), "/root").toString();
+	    webServer.setDataSource(dataSource);
+	    webServer.start();
+        return webServer.getWebAppUrl().toString();
     }
 
     private WebDriver createWebBrowser() {
--- src/test/java/com/exilesoft/exercise/WebServer.java
+++ src/test/java/com/exilesoft/exercise/WebServer.java
@@ -3,7 +3,11 @@ package com.exilesoft.exercise;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import org.eclipse.jetty.plus.jndi.EnvEntry;
 import org.eclipse.jetty.server.Server;
@@ -11,34 +15,88 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ShutdownHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.h2.jdbcx.JdbcDataSource;
-import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.exilesoft.exercise.company.type.CompanyType;
+import com.exilesoft.exercise.company.type.CompanyTypeRepository;
 
 
 public class WebServer {
 
-	public static void main(String[] args) throws Exception {
-		String shutdownToken = "sdgsdlgnslndglnsgdsgsdg";
-		int port = 10080;
+	private final Server server;
+	private final WebAppContext webApplication;
+	private String shutdownToken;
+	private final int port;
+	private final HandlerList handlers = new HandlerList();
 
-		shutdown(port, shutdownToken);
+	public WebServer(int port, String contextPath) {
+		this.port = port;
+		this.server = new Server(port);
 
-        System.setProperty(Environment.HBM2DDL_AUTO, "update");
+		this.webApplication = new WebAppContext("src/main/webapp", contextPath);
 
-        JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setURL("jdbc:h2:file:target/db/addressbook");
-        dataSource.setUser("sa");
-        new EnvEntry("java:/DefaultDS", dataSource);
-
-        Server server = new Server(port);
-		HandlerList handlers = new HandlerList();
-		handlers.addHandler(new ShutdownHandler(server, shutdownToken));
-		handlers.addHandler(new WebAppContext("src/main/webapp", "/"));
 		server.setHandler(handlers);
+	}
 
+	public void setDataSource(DataSource dataSource) throws NamingException {
+	    new EnvEntry("java:/DefaultDS", dataSource);
+	}
+
+	private void setShutdownToken(String shutdownToken) {
+		this.shutdownToken = shutdownToken;
+	}
+
+	public String getWebAppUrl() {
+		try {
+			return new URL("http", "localhost", server.getConnectors()[0].getLocalPort(),
+					webApplication.getContextPath()).toString();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void start() throws Exception {
+		if (shutdownToken != null) {
+			handlers.addHandler(new ShutdownHandler(server, shutdownToken));
+		}
+		handlers.addHandler(webApplication);
 		server.start();
 	}
 
-	private static void shutdown(int port, String shutdownToken) throws IOException {
+	public void setApplicationName(String applicationName) {
+	    getAppContext().getBean(ApplicationInfo.class).setName(applicationName);
+	}
+
+	public void setCompanyTypes(String... types) {
+	    CompanyTypeRepository typeRepository = getAppContext().getBean(CompanyTypeRepository.class);
+	    for (String type : types) {
+	        typeRepository.create(new CompanyType(type));
+	    }
+	}
+
+	private ApplicationContext getAppContext() {
+		return WebApplicationContextUtils.getRequiredWebApplicationContext(webApplication.getServletContext());
+	}
+
+	public static void main(String[] args) throws Exception {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL("jdbc:h2:file:target/db/addressbook");
+        dataSource.setUser("sa");
+		System.setProperty(AvailableSettings.HBM2DDL_AUTO, "update");
+
+		WebServer server = new WebServer(10080, "/");
+		server.setShutdownToken("sdgsdlgnslndglnsgdsgsdg");
+		server.attemptShutdown();
+		server.setDataSource(dataSource);
+		server.start();
+		server.setCompanyTypes("Software", "Offshoring");
+
+		System.out.println(server.getWebAppUrl());
+	}
+
+	private void attemptShutdown() throws IOException {
 		URL url = new URL("http", "localhost", port, "/shutdown?token=" + shutdownToken);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setRequestMethod("POST");
@@ -48,5 +106,6 @@ public class WebServer {
 			// OK: Not running
 		}
 	}
+
 
 }
